0566c99eac2b489eb722b4edbc40a02a6130c1ba
andrei <andrei-sergeich@yandex.ru>
2021-12-14 23:18:59 +0300

    write_redirect validating almost done (need to write subfunc), need to write read_redirect func, open fd for redirect and get command from input and save it in struct

--- srcs/validator.c
+++ srcs/validator.c
@@ -5,7 +5,7 @@ int	pass_whitespaces(char *input, int it)
 //	int	it;
 //
 //	it = 0;
-	while (ft_strchr("\t", input[it]) || ft_strchr(" ", input[it]))
+	while (input[it] && (ft_strchr("\t", input[it]) || ft_strchr(" ", input[it])))
 		it++;
 	return (it);
 }
@@ -30,12 +30,71 @@ int	counting_quotes(char *input, int *it, char quote)
 	{
 		if (input[*it] == quote)
 		{
-			(*it)++;
+//			(*it)++;
 			return (0);
 		}
 	}
 //	g_ext_stat = 258;
 	return (print_msg(1, "syntax error with unclosed quotes"));
+}
+
+int	read_redirect(char *input, int *it)
+{
+	if (input[*it] == '1')
+		return (1);
+	if (input[*it] == '0')
+		return (0);
+	return (0);
+}
+
+int	write_redirect(char *input, int *it)
+{
+	if (input[*it] == '\0' || input[*it] == '|')
+		return (print_msg(1, "syntax error near unexpected token `newline'"));
+	if (input[*it] == '<')
+		return (print_msg(1, "syntax error near unexpected token `<'"));
+	if (input[*it] == '>')
+	{
+		*it = pass_whitespaces(input, *it + 1);
+		if (input[*it] == '\0')
+			return (print_msg(1, \
+						"syntax error near unexpected token `newline'"));
+		if (input[*it] == '|')
+			return (print_msg(1, "syntax error near unexpected token `|'"));
+		if (input[*it] == '>')
+			return (print_msg(1, "syntax error near unexpected token `>'"));
+		if (input[*it] == '<')
+			return (print_msg(1, "syntax error near unexpected token `<'"));
+	}
+	else
+	{
+		*it = pass_whitespaces(input, *it);
+		if (input[*it] == '\0')
+			return (print_msg(1,\
+						"syntax error near unexpected token `newline'"));
+		if (input[*it] == '|')
+			return (print_msg(1, "syntax error near unexpected token `|'"));
+		if (input[*it] == '>')
+			return (print_msg(1, "syntax error near unexpected token `>'"));
+		if (input[*it] == '<')
+			return (print_msg(1, "syntax error near unexpected token `<'"));
+	}
+	return (0);
+}
+
+int	counting_redirect(char *input, int *it, char redirect)
+{
+	if (redirect == '>' && write_redirect(input, it))
+	{
+//		g_ext_stat = 258;
+		return (1);
+	}
+	if (redirect == '<' && read_redirect(input, it))
+	{
+//		g_ext_stat = 258;
+		return (1);
+	}
+	return (0);
 }
 
 int	validator(char *input)
@@ -54,11 +113,11 @@ int	validator(char *input)
 //		printf("%c\t%d\n", input[it], it);
 		if (input[it] == '\"' && counting_quotes(input, &it, input[it]))
 			return (1);
-//		if ((input[it] == '<' || input[it] == '>') && counting_quotes(input, &it, input[it]))
-//			return (1);
-		if (input[it] == '<' && find_file_name(input, &it))
-//			printf("it = %d\n", it);
+		if ((input[it] == '<' || input[it] == '>') && counting_redirect(input, &it, input[it]))
 			return (1);
+//		if (input[it] == '<' && find_file_name(input, &it))
+//			printf("it = %d\n", it);
+//			return (1);
 		it++;
 	}
 	return (0);
